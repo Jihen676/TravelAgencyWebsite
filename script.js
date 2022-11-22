a2530babe8386aced9def058c3b43aba84b66e54
jihen zemzem <jihenzemzem2@gmail.com>
2022-11-22 21:03:34 +0100

    modifyinngg

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let searchBtn =document.querySelector('#search-btn');
 let searchBar =document.querySelector('.search-bar-container');
+let formBtn =document.querySelector('#login-btn');
+let loginForm =document.querySelector('.login-form-container');
+let formClose =document.querySelector('#form-close');
 window.onscroll = () =>{
     searchBtn.classList.remove('fa-times');
     searchBar.classList.remove('active');
@@ -7,4 +10,10 @@ window.onscroll = () =>{
 searchBtn.addEventListener('click', () =>{
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
+});
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.toggle('active');
+});
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
 });
